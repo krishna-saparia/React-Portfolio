0bbb0f5b21012f77dd2c9733504deb522688523e
krishna saparia <krishnasaparia43045@gmail.com>
2021-01-08 18:35:44 -0500

    Add Bootstarp.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,10 @@
 import React from 'react';
+
+import Hero from '../components/Hero';
+
 function HomePage(props) {
     return(
-        <p>Home Page Works.</p>
+        <Hero title = {props.title} subTitle = {props.subTitle} text = {props.text} />
     );
 }
 
