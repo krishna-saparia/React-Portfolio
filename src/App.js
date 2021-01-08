aca7741c10f45bce915e438e36ed46724848dc48
krishna saparia <krishnasaparia43045@gmail.com>
2021-01-08 15:16:57 -0500

    Add Navbar with expand toggle

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import {BrowserRouter as Router , Route , Link } from 'react-router-dom';
+import { Container, Navbar ,Nav } from 'react-bootstrap';
+import './App.css';
 
 class App extends React.Component{
+ 
   constructor(props) {
+    
     super(props);
+    
     this.state = {
       title: 'Krishna Saparia',
       headerLinks: [
@@ -21,13 +27,31 @@ class App extends React.Component{
       contact: {
         title : 'Let\'s talk',
       }
-  }
+    }
   }
   
   
   render() {
     return (
-      <div> Hello react </div>
+      <Router>
+        <Container className = "p-0" fluid={true}>
+
+          <Navbar className = "border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Krishna Saparia</Navbar.Brand>
+            <Navbar.Toggle className="border-0"aria-controls="navbar-toggle"/>
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+
+
+
+        </Container>
+      </Router>
     );
   }
 }
