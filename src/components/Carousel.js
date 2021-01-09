0aa3b10ab0e7d9e07f5e33884f4b91d1da1dcc99
krishna saparia <krishnasaparia43045@gmail.com>
2021-01-09 14:06:48 -0500

    Create Carousel and cards and add Info.

---
+++ src/components/Carousel.js
@@ -0,0 +1,79 @@
+import React from 'react';
+
+import tv_2 from '../assets/Img/tv_2.png';
+import bag_1 from '../assets/Img/bag_1.png';
+import people_1 from '../assets/Img/people_1.png';
+import Card from './Card';
+import { Container, Row } from 'react-bootstrap';
+
+class Carousel extends React.Component {
+    constructor(props) {
+        super(props);
+
+
+        this.state = {
+            items: [
+                {
+                    id: 0,
+                    title: 'TV -Show',
+                    subTitle: 'The Show Details and Seasons Info.',
+                    imgSrc: tv_2,
+                    link: 'https://github.com/krishna-saparia/TV-Show-Express-Node-JS',
+                    selected: false
+                },
+                {
+                    id: 1,
+                    title: 'To shop Grocery List',
+                    subTitle: 'Create your own Grocery List for shopping..',
+                    imgSrc: bag_1,
+                    link: 'https://github.com/kanvisavani-discordbot/GroceryEJS',
+                    selected: false
+                },
+                {
+                    id: 2,
+                    title: 'Employee List',
+                    subTitle: 'The Web App to create and update employee details.',
+                    imgSrc: people_1,
+                    link: 'https://github.com/krishna-saparia/Employee-List-Frontend', 
+                    selected: false
+                },
+            ]
+            
+        }
+    }
+
+    handlecardClick = (id , card) => {
+        // Split Operator
+        let items = [...this.state.items];
+
+        items[id].selected = items[id].selected ? false : true;
+
+        items.forEach (item => {
+            if(item.id !== id){
+                item.selected = false;
+            } 
+        });
+
+        this.setState({
+            items
+        });
+    }
+
+    makeItems = (items) => {
+        return items.map(item => {
+            return <Card item = {item} onClick={(e => this.handlecardClick(item.id , e))} key = {item.id} />
+        })
+    }
+
+    render() {
+        return(
+            <Container fluid={true}>
+                <Row className = "justify-content-around">
+                    { this.makeItems(this.state.items) }
+                </Row>
+            </Container>
+        );
+    }
+}
+
+export default Carousel;
